13e14eff2d13f62e75dcf8f66f390c3fb28d1b1e
Vadim <vadim.beglov@gmail.com>
2014-07-15 19:07:51 -0400

    Fix в редиректах

--- wa-apps/site/lib/actions/redirect/siteRedirect.action.php
+++ wa-apps/site/lib/actions/redirect/siteRedirect.action.php
@@ -8,6 +8,14 @@ class siteRedirectAction extends waViewAction
         $domain = siteHelper::getDomain();
         $urls = waRedirect::getByDomain($domain);
 
+        $domain_replace_array = array(
+            'new.1mmtt.ru'=>'1mmtt.ru',
+            'l1mmtt'=>'1mmtt.ru',
+        );
+        $domain_replace = $domain;
+        if(isset($domain_replace_array[$domain]))$domain_replace = $domain_replace_array[$domain];
+        $this->view->assign('domain_replace', $domain_replace);
+
         $this->view->assign('urls', $urls);
         $this->view->assign('domain_id', $domain_id);
         $this->view->assign('domain', $domain);
