d6b3487df6be490c2cd7cfb4be1f11f26eea6de5
chelbeh@gmail.com <chelbeh@gmail.com>
2015-07-31 13:06:15 +0300

    метро

---
+++ wa-system/webasyst/lib/models/waMetro.model.php
@@ -0,0 +1,16 @@
+<?php
+
+class waMetroModel extends waModel
+{
+    protected $table = 'wa_metro';
+
+    public function all()
+    {
+        $stations = array();
+        $data = $this->order('name')->fetchAll();
+        foreach ($data as $d) {
+            $stations[$d['name']] = $d['name'];
+        }
+        return $stations;
+    }
+}
