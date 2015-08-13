30a4001cfa79d2b9702d1d8ad46ed1b28393d8b6
chelbeh@gmail.com <chelbeh@gmail.com>
2015-08-13 14:03:01 +0300

    адаптивная верстка для блога

--- wa-apps/site/themes/designsovkusom/js/custom_cart.js
+++ wa-apps/site/themes/designsovkusom/js/custom_cart.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
 
+    /*
     $(".cartheader, .cart-tot").live('click', function () {
         if (!$("#cart-summary").hasClass("empty")) {
             showCart();
@@ -9,46 +10,40 @@ $(document).ready(function () {
         return false;
 
     });
-
-
-    $("#fast_cart_view").overlay({
-        top: 0,
-        closeOnClick: true,
-        oneInstance: false,
-        load: false,
-        mask: {
-            color: '#000',
-            loadSpeed: 200,
-            opacity: 0.6,
-            zIndex: 2540
-        },
-        onClose: function () {
-            if ($(".cart_panel").hasClass("close")) {
-                $(".cart_panel").css('display', 'none');
-                $(".cart_panel").slideDown("slow");
-                $(".cart_panel").removeClass("close");
-            } else {
-                $(".cart_panel").slideUp("slow");
-                $(".cart_panel").addClass("close");
-            }
-            orderFinalized();
-            closeCart();
-        }
-    });
+     */
     infoHoverInit();
+    textareaInit();
+
 });
 
+function textareaInit() {
+    $(".wa-name").click(function () {
+        console.log("Debug");
+        $(this).addClass("active");
+        $(this).parents(".wa-field").find("input").focus();
+        $(this).parents(".wa-field").find("textarea").focus();
+    });
+}
 function clog(str) {
     if (window.console) console.log(str);
 }
 
 function Coupon() {
+    $("#coupon_input").val($("#coupon_input_fake").val());
     var data = $("#cart_form").serialize();
-    console.log(data);
     $.post('/cart/', data, function (result) {
         $.post('/cart/save/', {id: 0}, function (response) {
-            console.log(response);
             updatesCart(response.data);
+            if (response.data.discount_numeric > 0) {
+                $(".coupon_text").text("Ваш промокод активирован");
+                $(".coupon_text").removeClass("coupon_red");
+                $(".coupon_text").addClass("coupon_green");
+            }
+            else {
+                $(".coupon_text").text("Промокод введён не правильно");
+                $(".coupon_text").removeClass("coupon_green");
+                $(".coupon_text").addClass("coupon_red");
+            }
         }, "json");
     });
 };
@@ -56,8 +51,11 @@ function Coupon() {
 
 function showCart() {
     // $("body").addClass('freeze');
-    $("#fast_cart_view").overlay().load();
-    $("#cart_fast_info").html("<div class='loading'></div>");
+    $("#fast_cart_view").openModal();
+
+
+    var preloading = ' <div class="progress"><div class="indeterminate"></div></div>';
+    $("#cart_fast_info").append(preloading);
     $.get('/cart/', function (data) {
         var cont = $($.parseHTML(data)).find("#cart_content");
         makeCart(cont);
@@ -140,9 +138,20 @@ function minSumCheck() {
     }, "json");
 
 }
+$(document).ajaxComplete(function () {
+    Materialize.updateTextFields();
+});
 function basicAuthFormInit(block) {
+
     clog('basicAuthFormInit');
+    $('#modal1').on('click', function () {
+        $('#fast_cart_view').closeModal();
+    });
+
+
     setPhoneValidator('input[name="customer[phone]"]');
+
+
     minSumCheck();
     $("#order_create").click(function () {
         if ($(this).hasClass('deactivate')) {
@@ -159,54 +168,62 @@ function basicAuthFormInit(block) {
 }
 
 function setPhoneValidator(element) {
-    $(element).msk("+7 (999) 999-99-99", {empty_string: "7 ___ _______"});
+    $(element).msk("+7 (999) 999-99-99");
 
 }
 
 function createOrder(block) {
     clog('createOrder start');
-    // $(".overlay_container").css("height", "100%");
-    //$("#fast_cart_view").css("height", "730px");
-    var form = block.find("#cart_form");
-    var url = '/cart/';
-    var data = form.serialize() + '&checkout=1';
-    //console.log(data);
-    var checkout_form = block.find("#checkout_block").find('form');
-    var checkout_data = checkout_form.serialize();
+    Materialize.updateTextFields();
+    if ($("#base_form").valid() == false) {
+        return 0;
+    }
+    else {
+        var form = block.find("#cart_form");
+        var url = '/cart/';
+        var data = form.serialize() + '&checkout=1';
+        //console.log(data);
+        var checkout_form = block.find("#checkout_block").find('form');
+        var checkout_data = checkout_form.serialize();
 
-    var cart = $("#cart_content").detach();
+        // var cart = $("#cart_content").detach();
 
-    $.post(url, data, function (result) {
+        $.post(url, data, function (result) {
 
-        var res = $($.parseHTML(result));
-        var cart_cont = res.filter("#cart_content");
-        var basicauth_cont = res.find('#checkout_block');
-        if (cart_cont.length > 0) {
-            clog('error in cart data');
-            makeCart(cart_cont);
-        }
-        else if (basicauth_cont.length > 0) {
-            clog('save basic data');
-            $.post('/checkout/', checkout_data, function (result) {
-                var res = $($.parseHTML(result));
-                var address_cont = res.find('#checkout_address');
-                var basic_cont = res.find('#checkout_block');
-                clog(res);
-                if (address_cont.length > 0) {
-                    clog('data saved, init address form');
-                    makeAddress(address_cont);
-                }
-                if (basic_cont.length > 0) {
-                    //  alert('Извините, минимальная сумма заказа составляет 999 рублей');
-                    clog('error in basic data');
-                    $("#cart_fast_info").html(cart);
-                    $("#basicauth_block").html(basic_cont);
-                    basicAuthFormInit(block);
-                }
-            });
-        }
-    });
-    block.html("<div class='loading'></div>");
+            var res = $($.parseHTML(result));
+            var cart_cont = res.filter("#cart_content");
+            var basicauth_cont = res.find('#checkout_block');
+            if (cart_cont.length > 0) {
+                clog('error in cart data');
+                makeCart(cart_cont);
+            }
+            else if (basicauth_cont.length > 0) {
+                clog('save basic data');
+                $.post('/checkout/', checkout_data, function (result) {
+                    var res = $($.parseHTML(result));
+                    var address_cont = res.find('#checkout_address');
+                    var basic_cont = res.find('#checkout_block');
+                    clog(res);
+                    if (address_cont.length > 0) {
+                        clog('data saved, init address form');
+                        makeAddress(address_cont);
+                    }
+                    if (basic_cont.length > 0) {
+                        //  alert('Извините, минимальная сумма заказа составляет 999 рублей');
+                        clog('error in basic data');
+                        //$("#cart_fast_info").html(cart);
+                        $("#basicauth_block").html(basic_cont);
+                        basicAuthFormInit(block);
+                    }
+                });
+            }
+        });
+        var preloading = ' <div class="progress"><div class="indeterminate"></div></div>';
+
+        block.append(preloading);
+    }
+
+
 }
 
 
@@ -214,7 +231,7 @@ function makeAddress(cont) {
     clog('makeAddress');
 
     $("#cart_fast_info").html(cont);
-    $(".address_form").after("<div class='checkout_right'><div class='checkout_fast'><div class='checkout_shipping'></div><div class='checkout_billing'></div><div class='checkout_shipping_text hint'></div></div><div class='checkout_order_info'></div><div class='checkout_final'></div></div>");
+    $(".address_form").after("<div class='col l5 m5 s12'><div class='checkout_right'><div class='checkout_fast'><div class='checkout_shipping'></div><div class='checkout_billing'></div><!--div class='checkout_shipping_text hint'></div--></div><div class='checkout_order_info'></div><div class='checkout_final'></div></div></div></div>  ");
     $("input[name='customer[address.shipping][region]']").change(function () {
 
         updateRegion(cont);
@@ -223,36 +240,30 @@ function makeAddress(cont) {
     updateRegion(cont);
 
 
-    $(".cart_return").click(function () {
-        showCart();
-    });
     /*putShipmentDate($("input[name='order[shipping_date]']"));*/
 }
 
 function updateRegion(cont) {
     clog('updateRegion');
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+    //$('select').material_select();
 
-    var datepick = $('input[name="order[shipping_date]"]');
-    if (!datepick.hasClass("pickme")) {
-        datepick.pickmeup({
-            format: 'd.m.Y',
-            min: new Date(),
-            change: function (val) {
-                datepick.val(val).pickmeup('hide')
-            }
-        });
+    setPhoneValidator('input[name="customer[phone]"]');
+// изначально берем регион Geolocation
+    var region_id = $("#region_name").data("id");
+    var region_name = $("#region_name").text();
+    if ($("#address_form #region").val() == "") {
+        $("#region_name").removeData("id");
+        $("#address_form #region_text").val(region_name);
+        $("#address_form #region").val(region_id);
 
-        datepick.addClass("pickme")
 
     }
 
-// изначально берем регион Geolocation
-    /* var region_id = $("#region_name").data("id");
-     if ($("#address_form #region").val() == "") {
-     $("#address_form #region").val(region_id);
-     }
-     */
-    //Kladr();
+    //Kladr();s
     var region = $("#address_form #region").val();
 
     $("#address_form .field_dynamic").each(function () {
@@ -301,6 +312,7 @@ function loadShipping(cont) {
     var form = $("#address_form");
     var url = '/checkout/address/';
     var data = form.serialize();
+    textareaInit();
     $.post(url, data, function (result) {
         clog('data posted shipping');
         $.get('/checkout/shipping/', function (result) {
@@ -310,18 +322,37 @@ function loadShipping(cont) {
                 $(".checkout_shipping").first().html(res2);
                 $("input[type=radio]").click(function () {
                     selectShipping(cont);
+
+                    if ($(this).val() == 28) {
+                        $(".select_hermes").show();
+                    }
+                    else {
+                        $(".select_hermes").hide();
+                    }
                 });
                 var f = $("input[type=radio]").first();
                 f.prop('checked', true);
                 f.click();
+
             }
             else {
                 clog('error in shipping data');
             }
+            Materialize.updateTextFields();
+            var input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
+            $(document).on('change', input_selector, function () {
+                if ($(this).val().length !== 0 || $(this).attr('placeholder') !== undefined) {
+                    $(this).parents(".wa-field").first().children('.wa-name, i').addClass('active');
+                }
+                validate_field($(this));
+            });
         })
     });
+
 }
 function CheckForm() {
+
+
     $.extend($.validator.messages, {
         required: "Это поле необходимо заполнить.",
         remote: "Пожалуйста, введите правильное значение.",
@@ -344,23 +375,30 @@ function CheckForm() {
 
     var validator = $("#address_form").validate({
         rules: {
-            "customer[email]": {
-                required: true,
-                email: true
-            },
-            "customer[lastname]": {
-                required: true
-            },
+            /*
+             "customer[email]": {
+             required: true,
+             email: true
+             },
+             "customer[lastname]": {
+             required: true
+             },
+             "customer[firstname]": {
+             required: true
+             },
+             "customer[address.shipping][city]": {
+             required: true
+             },
+             "customer[address.shipping][street_name]": {
+             required: true
+             },
+             "order[shipping_date]": {
+             required: true
+             }*/
             "customer[firstname]": {
                 required: true
             },
-            "customer[address.shipping][city]": {
-                required: true
-            },
-            "customer[address.shipping][street_name]": {
-                required: true
-            },
-            "order[shipping_date]": {
+            "customer[phone]": {
                 required: true
             }
         },
@@ -398,6 +436,13 @@ function selectShipping(cont) {
     $(".checkout_shipping_text").html(text);
     $.post(url, data, function (result) {
         clog('shipping saved');
+        if ($("input[name='shipping_id']:checked").val() == 28) {
+            $(".select_hermes").show();
+        }
+        else {
+            $(".select_hermes").hide();
+        }
+        //$('select').material_select();
         // Kladr();
         var res2 = $($.parseHTML(result)).find("#payment_content");
         var res3 = $($.parseHTML(result)).find("#order_summary");
@@ -405,19 +450,18 @@ function selectShipping(cont) {
             $(".checkout_billing").html(res2);
             var f = res2.find("input[type=radio]").first();
             f.prop('checked', true);
-            $(".checkout_final").html("<div class='order_final'><input type='button' class='rv-button button big orange-button' value='Подтвердить заказ'></div>");
-
+            $(".checkout_final").html("<div class='order_final'><button value='Оформить заказ'  class='btn waves-effect waves-light  rv-button' type='submit' name='action'>Подтвердить заказ<i class='mdi-action-done-all right'></i></button> </div>");
             $(".checkout_order_info").html(res3);
             $(".checkout_final").find(".order_final").click(function () {
 
                 if (CheckForm()) {
                     finalizeOrder(cont);
-
                 }
             });
         } else {
             clog('error in saving shipping');
         }
+
     });
 }
 
@@ -432,32 +476,29 @@ function finalizeOrder(cont) {
         clog('data posted');
         var res_error = $($.parseHTML(result)).find("#checkout_address");
         var res_ok = $($.parseHTML(result)).find("#shipping_content");
-
-        if (res_error.length > 0) {
-            clog('error in address');
-            makeAddress(res_error);
-        }
-        else {
             var url = '/checkout/shipping/';
             var data = shipping_form.serialize() + '&place_order=1';
             $.post(url, data, function (result) {
-
                 var payment_form = $($.parseHTML(result)).find("#payment_form");
                 var url = '/checkout/payment/';
                 var data = payment_form.serialize();
                 $.post(url, data, function (result) {
                     var res_ok = $($.parseHTML(result)).find("#checkout_success");
-                    $("#cart_fast_info").html(res_ok);
+                    //$("#cart_fast_info").html(res_ok);
+                    orderFinalized();
                     return false;
                 });
             });
-        }
+
+
     });
-    cont.html("<div class='loading'></div>");
+    var preloading = ' <div class="progress"><div class="indeterminate"></div></div>';
+    cont.append(preloading);
 }
 
 function orderFinalized() {
-
+    location.href = '/checkout/success/';
+    return;
     $.post('/cart/save/', {id: 0}, function (response) {
         updatesCart(response.data);
     }, "json");
@@ -482,14 +523,14 @@ function infoHoverInit() {
 }
 
 function closeCart() {
-    $("#fast_cart_view").overlay().close();
-    $("body").removeClass('freeze');
+    $('#fast_cart_view').closeModal();
 }
 function updatesCart(data) {
     console.log('Updating cart...');
+
     var cart_total = data.total_numeric;
-    if (cart_total < 1000) {
-        $('.cart_wholesale').text('Извините, минимальная сумма заказа 1000 рублей.');
+    if (cart_total < 900) {
+        $('.cart_wholesale').text('Извините, минимальная сумма заказа 900 рублей.');
         $('#order_create').addClass('deactivate');
     }
     else {
@@ -520,8 +561,13 @@ function updatesCart(data) {
         $("#order_create").addClass("inactive");
 
         $(".cartheader").addClass("deactivate");
-        $(".cart_panel").hide();
-        closeCart();
+
+        if ($("#cart_content").length > 0) {
+            //$(".cart_panel").hide();
+            $('#modal1').closeModal();
+            $("#fast_cart_view").hide();
+        }
+
     }
 
     if (data.discount_numeric > 0) {
@@ -531,6 +577,6 @@ function updatesCart(data) {
         $(".discount_block").hide();
     }
 
-
+    Materialize.updateTextFields();
 }
 
