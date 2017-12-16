d0cb53fdd5ed8289770c7349abdccb0f18b92269
Vadim <chelbeh@gmail.com>
2017-12-16 10:40:50 +0300

    метро

--- wa-system/webasyst/lib/models/waMetro.model.php
+++ wa-system/webasyst/lib/models/waMetro.model.php
@@ -11,6 +11,32 @@ class waMetroModel extends waModel
         foreach ($data as $d) {
             $stations[$d['name']] = $d['name'];
         }
+
         return $stations;
+    }
+
+    /**
+     * Обновляет список станций метро
+     *
+     * @see shopUpdateMetroCli
+     * @throws waException
+     */
+    public function update()
+    {
+        $json = file_get_contents('https://api.hh.ru/metro/1');
+        $metro = json_decode($json);
+
+        if ($metro) {
+            foreach ($metro->lines as $line) {
+                foreach ($line->stations as $station) {
+                    $this->insert([
+                        'name' => $station->name,
+                        'line_id' => $line->id,
+                        'color' => $line->hex_color,
+                        'sort' => $station->order
+                    ], 1);
+                }
+            }
+        }
     }
 }
