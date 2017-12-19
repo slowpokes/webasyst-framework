98a7855f52288cc458ed001a1291e7d61bb10bcf
Vadim <chelbeh@gmail.com>
2017-12-19 17:06:53 +0300

    отключил netpeak

--- wa-system/debug/waDebug.class.php
+++ wa-system/debug/waDebug.class.php
@@ -1,20 +1,23 @@
 <?php
 
-class Debug {
+class Debug
+{
 
     static $objects;
     static $timer;
 
-    static function enabled(){
-        if(isset($_COOKIE['show_debug'])){
-            if($_COOKIE['show_debug']>=1)
+    static function enabled()
+    {
+        if (isset($_COOKIE['show_debug'])) {
+            if ($_COOKIE['show_debug'] >= 1)
                 return true;
         }
         return false;
     }
 
-    static function query($sql, $result, $error_code, $error, $debug_backtrace){
-        if(self::enabled()){
+    static function query($sql, $result, $error_code, $error, $debug_backtrace)
+    {
+        if (self::enabled()) {
             $time_end = microtime(1);
             $time = $time_end - self::$timer;
 
@@ -30,25 +33,29 @@ class Debug {
         }
     }
 
-    static function queryStart(){
-        if(self::enabled()){
+    static function queryStart()
+    {
+        if (self::enabled()) {
             self::$timer = microtime(1);
         }
     }
 
-    static function describe($table, $keys){
+    static function describe($table, $keys)
+    {
     }
 
-    static function sql($sql){
+    static function sql($sql)
+    {
     }
 
-    private static function getViewBacktrace($arr){
+    private static function getViewBacktrace($arr)
+    {
         $backtrace = '';
         $n = 0;
-        foreach($arr as $b){
+        foreach ($arr as $b) {
             $file = $b['file'];
             $line = $b['line'];
-            if($n>0){
+            if ($n > 0) {
                 $backtrace .= "$file ($line) -> ";
             }
             $n++;
@@ -56,24 +63,27 @@ class Debug {
         return $backtrace;
     }
 
-    private static function getViewQuery($obj){
+    private static function getViewQuery($obj)
+    {
         $html = "<div class='query'>{$obj['query']}</div>";
-        $html .= "<div class='time'>".sprintf("%01.5f", $obj['time'])."</div>";
-        $html .= "<div class='backtrace'>".self::getViewBacktrace($obj['backtrace'])."</div>";
-        if($obj['err_str']!=''){
+        $html .= "<div class='time'>" . sprintf("%01.5f", $obj['time']) . "</div>";
+        $html .= "<div class='backtrace'>" . self::getViewBacktrace($obj['backtrace']) . "</div>";
+        if ($obj['err_str'] != '') {
             $html = "<div class='query_error'>$html</div>";
         }
         return $html;
     }
 
-    private static function getVariableQuery($obj){
+    private static function getVariableQuery($obj)
+    {
         $html = "<div class='query'>{$obj['var']}</div>";
-        $html .= "<div class='backtrace'>".self::getViewBacktrace($obj['backtrace'])."</div>";
+        $html .= "<div class='backtrace'>" . self::getViewBacktrace($obj['backtrace']) . "</div>";
         return $html;
     }
 
-    static function variable($obj){
-        if(self::enabled()){
+    static function variable($obj)
+    {
+        if (self::enabled()) {
             $tv = array();
             $tv['type'] = 'variable';
             $tv['var'] = $obj;
@@ -82,56 +92,62 @@ class Debug {
         }
     }
 
-    static function getHtml(){
-        if(self::enabled()){
+    static function getHtml()
+    {
+        if (self::enabled()) {
             $str = '';
             $str .= '<link href="/debug.css" rel="stylesheet" type="text/css">';
             $str .= "<div class='debug'>";
-            foreach(self::$objects as $obj){
-                if($obj['type']=='query')       $str .= self::getView(self::getViewQuery($obj));
-                if($obj['type']=='variable')    $str .= self::getView(self::getVariableQuery($obj));
+            foreach (self::$objects as $obj) {
+                if ($obj['type'] == 'query') $str .= self::getView(self::getViewQuery($obj));
+                if ($obj['type'] == 'variable') $str .= self::getView(self::getVariableQuery($obj));
             }
             $str .= "</div>";
             return $str;
         }
     }
 
-    private static function getView($html){
+    private static function getView($html)
+    {
         return "<div class='debug_obj'>$html</div>";
     }
 
-    static function breakpoint(){
+    static function breakpoint()
+    {
         throw new waException("Breakpoint", 404);
     }
 
-    static function trace(){
+    static function trace()
+    {
         echo "<pre>";
         debug_print_backtrace();
         echo "</pre>";
         die();
     }
 
-    static function showQueries(){
-        if(self::enabled()){
-            foreach(self::$objects as $obj){
-                if($obj['type']=='query'){
-                    echo $obj['query']."\n";
-                    echo sprintf("%01.5f", $obj['time'])."\n\n";
+    static function showQueries()
+    {
+        if (self::enabled()) {
+            foreach (self::$objects as $obj) {
+                if ($obj['type'] == 'query') {
+                    echo $obj['query'] . "\n";
+                    echo sprintf("%01.5f", $obj['time']) . "\n\n";
                 }
             }
         }
     }
 
-    static function showTime(){
+    static function showTime()
+    {
         $backtrace = debug_backtrace();
         $line_str = '';
         //print_r($backtrace);
-        if(isset($backtrace[0])){
-            $line_str = basename($backtrace[0]['file'])."::".$backtrace[0]['line'];
+        if (isset($backtrace[0])) {
+            $line_str = basename($backtrace[0]['file']) . "::" . $backtrace[0]['line'];
         }
         global $_gl_time_start;
         $_gl_time_end = microtime(1);
-        $time = (intval(($_gl_time_end - $_gl_time_start)*1000))/1000;
-        echo "Timer: ".$time." sec, file: $line_str\n";
+        $time = (intval(($_gl_time_end - $_gl_time_start) * 1000)) / 1000;
+        echo "Timer: " . $time . " sec, file: $line_str\n";
     }
 }
