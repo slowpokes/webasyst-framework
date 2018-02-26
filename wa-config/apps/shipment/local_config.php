a1db1318cb78794b154d9a818d12022e777db73f
Vadim <chelbeh@gmail.com>
2018-02-26 12:09:23 +0300

    ems

---
+++ wa-config/apps/shipment/local_config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'updater_ids' => ['sdta1', 'cdekland', 'cdekair'],
+    'updater_cities_databases' => array(
+        'default' => array(
+            'price_plus' => 50,
+        ),
+    ),
+);
