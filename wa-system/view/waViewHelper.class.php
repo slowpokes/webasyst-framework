6013b7b8bebf912447bbdd62c033459c70949246
Vadim <vadim.beglov@gmail.com>
2019-09-18 14:58:56 -0400

    fix

--- wa-system/view/waViewHelper.class.php
+++ wa-system/view/waViewHelper.class.php
@@ -213,7 +213,7 @@ class waViewHelper
             if (!empty($domain_config['google_analytics']['code'])) {
                 if (!empty($domain_config['google_analytics']['universal'])) {
                     $html .= <<<HTML
-<script type="text/javascript">
+<script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
