688febd5904252967baa96729df826c5b381f895
Vadim <vadim.beglov@gmail.com>
2020-03-31 09:16:07 -0400

    адрес сайта

--- wa-system/view/waViewHelper.class.php
+++ wa-system/view/waViewHelper.class.php
@@ -301,6 +301,7 @@ HTML;
 
     public function accountName($escape = true)
     {
+        return $this->appName($escape);//VADIM CODE
         $name = wa()->getSetting('name', 'Webasyst', 'webasyst');
         return $escape ? htmlspecialchars($name) : $name;
     }
